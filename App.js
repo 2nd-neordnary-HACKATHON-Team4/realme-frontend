ad72abc712a50a3985c194b5489be6ab2c775b0a
유다연 <udayeon@naver.com>
2022-09-25 07:24:40 +0900

    category api 연결, categoryList UI 변경, recoil 연결 (#20)

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {OnBoardingRoutes, RootRoutes, LoginRoutes} from './src/screens/Routes';
+import {RecoilRoot} from 'recoil';
 import HomeScreen from './src/screens/Feed/HomeScreen';
 import MyPageScreen from './src/screens/MyPageScreen';
 import {OnBoardingRoutes, RootRoutes, LoginRoutes} from './src/screens/Routes';
@@ -19,17 +21,19 @@ const App = () => {
     }
   });
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-        initialRouteName="Root">
-        <Stack.Screen name="LogIn" component={LoginRoutes} />
-        <Stack.Screen name="Root" component={RootRoutes} />
-        <Stack.Screen name="OnBoarding" component={OnBoardingRoutes} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <RecoilRoot>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+          initialRouteName="Root">
+          <Stack.Screen name="LogIn" component={LoginRoutes} />
+          <Stack.Screen name="Root" component={RootRoutes} />
+          <Stack.Screen name="OnBoarding" component={OnBoardingRoutes} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </RecoilRoot>
   );
 };
 

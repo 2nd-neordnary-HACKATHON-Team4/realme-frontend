ac48cd676393b476fdf0a35e13b80b73429cec30
HJ Kim <pknojlh27@naver.com>
2022-09-25 07:19:15 +0900

    login api

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-// import HomeScreen from './src/screens/Feed/HomeScreen';
-// import MyPageScreen from './src/screens/MyPageScreen';
+import HomeScreen from './src/screens/Feed/HomeScreen';
+import MyPageScreen from './src/screens/MyPageScreen';
 import {OnBoardingRoutes, RootRoutes, LoginRoutes} from './src/screens/Routes';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  useEffect(() => {
+    try {
+      setTimeout(() => {
+        SplashScreen.hide();
+      }, 2000); //스플래시 활성화 시간 2초
+    } catch (e) {
+      console.log(e.message);
+    }
+  });
   return (
     <NavigationContainer>
       <Stack.Navigator
